9717a920ed9d2dc6c3c81957e51cfa7b6e9a1d32
santiagopr123 <santiagopr@udea.edu.co>
2021-09-24 23:55:07 -0500

    se metio

---
+++ CodigoP2Qt/imageresized.h
@@ -0,0 +1,31 @@
+#ifndef IMAGERESIZED_H
+#define IMAGERESIZED_H
+
+#include <QImage>
+#include <QDebug>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+class ImageResized
+{
+    string Filename;
+    QImage Img;
+    ofstream Archivo;
+
+public:    
+    ImageResized();
+    ImageResized(string Nombre);
+
+    void PonerNombre(string Nombre);
+    string ObtenerNombre();
+
+    void Deteccion_Tipo_Muestreo();
+
+    void Sobremuestreo();
+    void submuestreo();
+    void EnvioDatos(short int MatrizR[][16],short int MatrizG[][16],short int MatrizB[][16]);
+};
+
+#endif // IMAGERESIZED_H
